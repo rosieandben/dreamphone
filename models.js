3967f5c26ef0d17ac3e8bcf855a7a9c0bf7a241b
Simon Hilder <simon.hilder@bjss.com>
2022-04-21 13:05:27 +0100

    Added models file with data objects and initial boys

---
+++ models.js
@@ -0,0 +1,63 @@
+const AnswerAbout = {
+    Location: 'Location',
+    Sport: 'Sport',
+    Food: 'Food',
+    Clothes: 'Clothes'
+}
+
+const Sport = {
+    Football: 'Football',
+    Skateboarding: 'Skateboarding',
+    Volleyball: 'Volleyball',
+    Surfing: 'Surfing',
+    Basketball: 'Basketball',
+    Tennis: 'Tennis'
+}
+
+const Food = {
+    Biscuits: 'Biscuits',
+    IceCream: 'Ice cream',
+    HotDogs: 'Hot dogs',
+    Pizza: 'Pizza',
+    Sweets: 'Sweets',
+    Popcorn: 'Popcorn'
+}
+
+const Location = {
+    Mall: 'Mall',
+    SnackShop: 'Snack Shop',
+    Movies: 'Movies',
+    Park: 'Park',
+    Beach: 'Beach',
+    Gym: 'Gym'
+}
+
+const Clothes = {
+    Jeans: 'Jeans',
+    Tie: 'Tie',
+    Jacket: 'Jacket',
+    Glasses: 'Glasses',
+    Hat: 'Hat',
+    Yellow: 'Yellow'
+}
+
+export const boys = [
+    {
+        name: 'Dave',
+        number: '555-1111',
+        answerAbout: AnswerAbout.Location,
+        location: Location.Mall,
+        sport: null,
+        food: Food.Biscuits,
+        clothes: Clothes.Jeans
+    },
+    {
+        name: 'George',
+        number: '555-1233',
+        answerAbout: AnswerAbout.Location,
+        location: Location.Mall,
+        sport: null,
+        food: Food.IceCream,
+        clothes: Clothes.Tie
+    }
+]
