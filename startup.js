6f0ca9067fe08dda43170ab375b792722e46c5be
Simon Hilder <simon.hilder@bjss.com>
2022-04-28 18:42:19 +0100

    Added videos and incorporated into startup code

--- startup.js
+++ startup.js
@@ -1,4 +1,5 @@
 import { AnswerAbout, boys } from './models.js';
+import fs from 'fs';
 
 export const runStartup = () => {
     const startupBoys = JSON.parse(JSON.stringify(boys));
@@ -13,19 +14,32 @@ const chooseAdmirer = (startupBoys) => {
     return startupBoys[randomIndex];
 }
 
+const shuffleCollection = (collection) => {
+    return collection.sort(() => Math.random() - 0.5);
+}
+
 const allocateVideos = (startupBoys) => {
+    const locationVideos = shuffleCollection(fs.readdirSync('./resources/video/Location'));
+    const sportVideos = shuffleCollection(fs.readdirSync('./resources/video/Sport'));
+    const foodVideos = shuffleCollection(fs.readdirSync('./resources/video/Food'));
+    const clothesVideos = shuffleCollection(fs.readdirSync('./resources/video/Clothes'));
+
     for (const boy of startupBoys) {
         switch (boy.answerAbout) {
             case AnswerAbout.Location:
+                boy.gameAttributes.allocatedVideo = locationVideos.pop();
                 break;
 
             case AnswerAbout.Sport:
+                boy.gameAttributes.allocatedVideo = sportVideos.pop();
                 break;
 
             case AnswerAbout.Food:
+                boy.gameAttributes.allocatedVideo = foodVideos.pop();
                 break;
 
             case AnswerAbout.Clothes:
+                boy.gameAttributes.allocatedVideo = clothesVideos.pop();
                 break;
         }
     }
