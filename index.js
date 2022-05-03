f48c8424141d449514a21d3f080855e5046e0d87
Simon Hilder <simon.hilder@bjss.com>
2022-05-03 12:06:09 +0100

    Disabling zoom on mobile

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { runStartup, startupBoys } from './startup.js';
 
+// disable zoom on mobile
+document.addEventListener('touchmove', e => {
+	if (e.touches.length > 1) {  
+		e.preventDefault();
+	}
+}, {passive: false});
+
 runStartup();
 
 let count = 0;
